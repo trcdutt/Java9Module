b1ac6e59234d73a3ba1c13a0ffd021be44b174ce
RAMESH <RAMESH@DESKTOP-PLQ8UJV>
2018-01-27 10:57:41 -0700

    First Rev

---
+++ src/main/java/com/trc/mainmodule/App.java
@@ -0,0 +1,18 @@
+package com.trc.mainmodule;
+
+//import com.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//        NameUtil n = new NameUtil();        
+//        System.out.println( "Hello World!" + n.getName() );
+        
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author RAMESH
+ *
+ */
+module mainmodule {
+	exports com.trc.mainmodule;
+//	requires junit;
+//	requires dependent;
+	
+}
